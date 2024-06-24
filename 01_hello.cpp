9e943e4ac1de926fb8346f479e00408dfbca46a8
Eshan <eshanbit101@gmail.com>
2024-06-24 16:03:15 +0530

    files renamed and organised

---
+++ 01_hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// using namespace std;
+
+int main(){
+    std::cout<<"I like dosa\n";
+    // '\n' is used to add a blank line
+    std::cout<<"It's really good" << std::endl;
+    // endl is also used to add a blank line
+    /*
+        This is a multi line comment
+        a 
+        new 
+        comment 
+    */
+    return 0;
+}
